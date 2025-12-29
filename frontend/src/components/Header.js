4000b0139f3bae8d5c73ebe127e987c9e8979389
Htech2005 <mikponhouelerichharold@gmail.com>
2025-12-29 17:06:10 +0100

    fix: remove SV links from Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -16,7 +16,6 @@ export default function Header() {
         <nav className="menu">
           <Link to="/">Accueil</Link>
           <Link to="/cdv">CDV</Link>
-          <Link to="/sv">SV</Link>
           <Link to="/tr">TR</Link>
           <Link to="/cv">CV</Link>
           <Link to="/dashboard">Dashboard</Link>
@@ -34,7 +33,6 @@ export default function Header() {
       <div className={`menu-mobile ${menuOpen ? "active" : ""}`}>
         <Link to="/" onClick={toggleMenu}>Accueil</Link>
         <Link to="/cdv" onClick={toggleMenu}>CDV</Link>
-        <Link to="/sv" onClick={toggleMenu}>SV</Link>
         <Link to="/tr" onClick={toggleMenu}>TR</Link>
         <Link to="/cv" onClick={toggleMenu}>CV</Link>
         <Link to="/dashboard" onClick={toggleMenu}>Dashboard</Link>
